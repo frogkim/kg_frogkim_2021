a6dab91931996e1a6b92231a48549c678ce45d9f
frogkim <66743695+frogkim@users.noreply.github.com>
2021-05-03 00:25:24 -0500

    Change loop from while to for. Add change numbers to string.

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 var message = ""
-let i=0;
-while(true)
+for(let i=2, isNumber=true;;)
 {
-    console.log("argv ", i.toString(), ": ", process.argv[i], "data type is ", typeof process.argv[i]);
-    message += process.argv[i];
-
-    if(++i < process.argv.length)
+    for(let j=0; j<process.argv[i].length; j++)
     {
-        message += ",";
+        if(process.argv[i][j] == '0'){
+            message += "Zero";
+        }   else if(process.argv[i][j] == '1'){
+            message += "One";
+        }   else if(process.argv[i][j] == '2'){
+            message += "Two";
+        }   else if(process.argv[i][j] == '3'){
+            message += "Three";
+        }   else if(process.argv[i][j] == '4'){
+            message += "Four";
+        }   else if(process.argv[i][j] == '5'){
+            message += "Five";
+        }   else if(process.argv[i][j] == '6'){
+            message += "Six";
+        }   else if(process.argv[i][j] == '7'){
+            message += "Seven";
+        }   else if(process.argv[i][j] == '8'){
+            message += "Eight";
+        }   else if(process.argv[i][j] == '9'){
+            message += "Nine";
+        }   else {
+            message = "Please enter NUMBERS as arguments";
+            console.log(i,j);
+            console.log(process.argv[i][j]);
+            isNumber = false;
+            break;
+        }    
     }
-    else
+
+    if(!isNumber){
         break;
+    }
+
+    if(++i < process.argv.length){
+        message += ",";
+    } else {
+        break;
+    }
 }
 
 if(message == ""){
-    console.log("Please enter argument.");
+    console.log("Please enter arguments.");
 } else{
     console.log(message)
 }
